1f8a67d3b87aef336d1293ae5cecc47519842a52
henry <henry@iMac-home.local>
2021-09-10 22:50:25 +0900

    multiplayer match

--- client/src/client.js
+++ client/src/client.js
@@ -1,22 +1,40 @@
-const socket = io();
-
 const writeEvent = (text) => {
+  // <ul> element
   const parent = document.querySelector("#events");
+
+  // <li> element
   const el = document.createElement("li");
   el.innerHTML = text;
+
   parent.appendChild(el);
 };
-writeEvent("Welcome to RPS");
-
-socket.on("message", writeEvent);
 
 const onFormSubmitted = (e) => {
   e.preventDefault();
+
   const input = document.querySelector("#chat");
   const text = input.value;
   input.value = "";
-  socket.emit("message", text);
+
+  sock.emit("message", text);
 };
+
+const addButtonListeners = () => {
+  ["rock", "paper", "scissors"].forEach((id) => {
+    const button = document.getElementById(id);
+    button.addEventListener("click", () => {
+      sock.emit("turn", id);
+    });
+  });
+};
+
+writeEvent("Welcome to RPS");
+
+const sock = io();
+sock.on("message", writeEvent);
+
 document
   .querySelector("#chat-form")
   .addEventListener("submit", onFormSubmitted);
+
+addButtonListeners();
--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 import http from "http";
 import express from "express";
 import socketio from "socket.io";
+import RpsGame from "./rps-game";
 
 const clientPath = `${__dirname}/../client`;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+let waitingPlayer = null;
 
 app.use(express.static(clientPath));
 
@@ -18,7 +21,14 @@ server.on("error", (err) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("someone connected");
+  if (!waitingPlayer) {
+    //start game
+    waitingPlayer = socket;
+    waitingPlayer.emit("message", "waiting for an oppenent");
+  } else {
+    new RpsGame(waitingPlayer, socket);
+    waitingPlayer = null;
+  }
   socket.emit("message", "Hi,everyone you are connected");
   socket.on("message", (text) => {
     io.emit("message", text);
